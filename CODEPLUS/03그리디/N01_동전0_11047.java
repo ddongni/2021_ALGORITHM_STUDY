142fce8262ff5f6cc2d487dd1204d39e886add74
ddongni <68309632+ddongni@users.noreply.github.com>
2021-02-01 16:59:45 +0900

    Create N01_동전0_11047.java

---
+++ CODEPLUS/03그리디/N01_동전0_11047.java
@@ -0,0 +1,23 @@
+package N01_동전0_11047;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int answer = 0;
+		for (int i = n - 1; i >= 0; i--) {
+			if (k / arr[i] >= 1) {
+				answer += k / arr[i];
+				k -= arr[i] * (k / arr[i]);
+			}
+		}
+		System.out.println(answer);
+	}
+}
