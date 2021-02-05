237d25eec6da7f6cf4960a79383b8f7f98eefad6
ddongni <68309632+ddongni@users.noreply.github.com>
2021-02-05 14:39:05 +0900

    Create N03_퇴사2_15486.java

---
+++ CODEPLUS2/05다이나믹프로그래밍/N03_퇴사2_15486.java
@@ -0,0 +1,29 @@
+package N03_퇴사2_15486;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] t = new int[n], p = new int[n];
+		for (int i = 0; i < n; i++) {
+			t[i] = sc.nextInt();
+			p[i] = sc.nextInt();
+		}            
+		int answer = Integer.MIN_VALUE;
+		int[] dp = new int[n+1];
+		for (int i = 0; i < n; i++) {
+			if (i > 0) {
+				dp[i] = Math.max(dp[i], dp[i - 1]);
+			}
+			if (i + t[i] >n)
+				continue;
+			dp[i + t[i]] = Math.max(dp[i + t[i]], dp[i] + p[i]);
+		}
+		for (int i = 0; i <= n; i++) {
+			answer = Math.max(answer, dp[i]);
+		}
+		System.out.println(answer);
+	}
+}
