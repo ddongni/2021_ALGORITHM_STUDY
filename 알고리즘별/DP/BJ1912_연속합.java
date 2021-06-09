67b1fdc5b5814682ecfd7e4d7b8b82818a3304c2
신동은 <68309632+ddongni@users.noreply.github.com>
2021-06-09 11:24:28 +0900

    Create BJ1912_연속합.java

---
+++ 알고리즘별/DP/BJ1912_연속합.java
@@ -0,0 +1,18 @@
+package BJ1912_연속합;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0;
+		int ans = Integer.MIN_VALUE;
+		for (int i = 0; i < n; i++) {
+			int num = sc.nextInt();
+			sum = num > sum + num ? num : sum + num;
+			ans = Math.max(ans, sum);
+		}
+		System.out.println(ans);
+	}
+}
